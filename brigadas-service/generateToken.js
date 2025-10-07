b6170242465feec10c3dc6a6306c076be040d6d8
sslcristian <catencia1@udi.edu.co>
2025-10-07 10:41:44 -0500

    Update generateToken.js

--- brigadas-service/generateToken.js
+++ brigadas-service/generateToken.js
@@ -5,7 +5,7 @@ const token = jwt.sign(
   {
     id_usuario: 1,
     nombre: "Administrador General",
-    rol: "administrador"  // Usa el mismo rol que espera tu middleware
+    rol: "admin"  // Usa el mismo rol que espera tu middleware
   },
   "brigadas_secret_key_IFN_2025", // ✅ Clave secreta correcta del servicio brigadas
   { expiresIn: "1d" } // Token válido por 1 día
