bbdd45c6618ba6ec89f82a5b1b2663bde8ee4403
AlejoX0 <mvb049@gmail.com>
2025-10-09 20:11:06 -0500

    Register terminado

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom'; // <-- 1. IMPORTA BrowserRouter
 import './index.css';
-
-// 1. Importa SOLAMENTE tu componente de la página de registro
 import RegisterPage from './pages/RegisterPage';
 
-// 2. Renderiza tu componente directamente
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RegisterPage /> {/* <-- Aquí le decimos que muestre tu página */}
+    {/* 2. ENVUELVE tu página con el componente BrowserRouter */}
+    <BrowserRouter>
+      <RegisterPage />
+    </BrowserRouter>
   </React.StrictMode>
 );
